fbdeb7732b7734744d8d09c013d2be3748467524
Taine Collins <tainecollins@Taines-MacBook-Pro.local>
2019-07-27 21:16:24 +1200

    shopping list

--- src/App.js
+++ src/App.js
@@ -22,13 +22,17 @@ class shoppinglist extends Component {
       {
         id: 4,
         text: 'flour'
+      },
+      {
+        id: 5,
+        text: 'cheese'
       }
     ];
   }
 
   render(){
     return(
-        <div className="wrap">
+        <div className="wrap container">
           <h1>Shopping List</h1> 
           <ul className="list-group">
             {
@@ -45,12 +49,11 @@ class shoppinglist extends Component {
             }
           </ul>
           <div className="new-item">
-            <button type="submit" className="add-item">Add Item</button>
+            <button type="submit" className="add-item" onClick={() => alert('I want to create a new list item but i dont know how. YET!!!!!!')}>Add Item</button>
           </div>
         </div>
     );
   }
-    
 }
 
 export default shoppinglist;
