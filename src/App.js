4cb85d6f133435992a6f310720d161ffeb978889
Taine Collins <932400105@cit3-501m-12.acg.ac.nz>
2019-07-30 15:50:59 +1200

    shopping list 30/7

--- src/App.js
+++ src/App.js
@@ -5,31 +5,42 @@ class shoppinglist extends Component {
 
   constructor(props){
     super(props);
-    this.items = [
-      {
-        id: 1,
-        text: 'milk'
-      },
-      {
-        id: 2,
-        text: 'bread'
-      },
-      {
-        id: 3,
-        text: 'eggs'
-      },
-      {
-        id: 4,
-        text: 'flour'
-      },
-      {
-        id: 5,
-        text: 'cheese'
-      }
-    ];
+
+    this.state = {
+      list: [],
+      listInputs: ''
+    };
   }
 
+  //this is to store the value
 
+  listInputValue = (e) => {
+    this.setState({listInputs: e.target.value})
+  }
+
+  // this will add a new item to the list
+
+  addListItem = (e) => {
+    e.preventDefault();
+
+    var listItem = {
+      id: Date.now(),
+      text: this.state.listInputs
+    }
+
+    var shoppingList = [...this.state.list,listItem];
+
+    this.setState({
+      list: shoppingList,
+      listInputs:''
+    })
+  }
+
+  removeListItem = (listId) => {
+    var list = this.state.list;
+    var filteredList = list.filter(function(item){return item.id !=listId});
+    this.setState({list:filteredList});
+  }
 
   render(){
     return(
@@ -37,27 +48,28 @@ class shoppinglist extends Component {
           <h1>Shopping List</h1> 
           <ul className="list-group">
             {
-              this.items.map(function(item){
+              this.state.list.map((listItem) => {
                 return(
-                  <li className="list-group-item" key={item.id}>
+                  <li className="list-group-item" key={listItem.id}>
                     <div className="item">
-                      {item.text}
+                      <input type="checkbox" className="checkbox"/>
+                      {listItem.text}
                     </div>
-                    <i className="far fa-times-circle note-remove"></i>
+                    <i onClick={(e) => {this.removeListItem(listItem.id)}} className="far fa-times-circle note-remove"></i>
                   </li>
                 );
               })
             }
-
-            <li className="new-item list-group-item">
-              <input type="text" className="input-item" id="list-input" placeholder="new item..."/>
-              <button type="submit" className="add-item">Add Item</button>
-            </li>
-
+            <form className="new-shopping-item">
+              <li className="new-item list-group-item">
+                <input type="text" className="input-item" id="list-input" placeholder="new item..." value={this.state.listInputs} onChange={this.listInputValue}/>
+                <button id="add-item" type="submit" className="add-item-btn" onClick={this.addListItem}>Add Item</button>
+              </li>
+            </form>
           </ul>
-          
-          
         </div>
+
+        
     );
   }
 }
